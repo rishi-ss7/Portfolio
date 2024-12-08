3155b404c3440b9c5a1834e8c379e47b13df79a3
rishi-ss7 <rishikasaudhan5152@gmail.com>
2024-12-09 00:07:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var tablink = document.getElementsByClassName("tab-links");
+// var tabcontaint = document.getElementsByClassName("tab-containts");
+// function opentab(tabname){
+//     for(tablink of tab-links){
+//         tablink.classList.remove("active-link");
+//     }
+//     for(tabcontaint of tab-contains){
+//         tabcontaint.classList.remove("active-tab");
+//     }
+// }
