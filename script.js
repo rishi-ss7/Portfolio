f06eb2bf24bd218328941762b77c3c18b9d98404
rishi-ss7 <rishikasaudhan5152@gmail.com>
2024-12-09 22:02:48 +0530

    four

--- script.js
+++ script.js
@@ -8,3 +8,10 @@
 //         tabcontaint.classList.remove("active-tab");
 //     }
 // }
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right ="0";
+}
+function closemenu(){
+    sidemenu.style.right ="-200px";
+}
